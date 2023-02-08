2aa1e3cf5199a228f7d48f41140c8e4734a18090
Alex5563 <alex.huang3651@g.gcpsk12.org>
2023-02-07 20:11:11 -0500

    Merge remote-tracking branch 'origin/main'

---
+++ app/src/main/java/com/example/exerme/DashboardPeterAndAlex.kt
@@ -0,0 +1,17 @@
+package com.example.exerme
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class DashboardPeterAndAlex : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun buttonClick(view: View?){
+        println("Button Clicked!")
+
+    }
+}
