29f6cff805180c98c14c27030769a5f8ff9884cd
Ye-june <Peterkim7290@gmail.com>
2023-02-08 18:56:56 -0500

    Adding a button for changing from Dashboard to Calendar

--- app/src/main/java/com/example/exerme/DashboardActivity.kt
+++ app/src/main/java/com/example/exerme/DashboardActivity.kt
@@ -1,11 +1,18 @@
 package com.example.exerme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        val calendarButton = findViewById<Button>(R.id.calendarButton)
+
+        calendarButton.setOnClickListener(View.OnClickListener { startActivity(Intent(this, CalendarActivity::class.java)) })
     }
 }
