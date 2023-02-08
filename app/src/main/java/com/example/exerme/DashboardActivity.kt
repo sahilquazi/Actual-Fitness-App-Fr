a80684e60d55793e03cf67e3883b4837c896758d
Alex5563 <alex.huang3651@g.gcpsk12.org>
2023-02-08 18:35:56 -0500

    Changed the names

--- app/src/main/java/com/example/exerme/DashboardActivity.kt
+++ app/src/main/java/com/example/exerme/DashboardActivity.kt
@@ -1,17 +1,12 @@
 package com.example.exerme
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun buttonClick(view: View?){
-        println("Button Clicked!")
 
     }
 }
