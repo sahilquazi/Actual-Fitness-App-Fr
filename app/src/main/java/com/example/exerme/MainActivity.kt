bcc44226d127182815d538975f5c534fdd661a70
nguyenvo5 <tinhquoc218@gmail.com>
2023-02-09 18:47:27 -0500

    calendar

--- app/src/main/java/com/example/exerme/MainActivity.kt
+++ app/src/main/java/com/example/exerme/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.example.exerme
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,9 +12,12 @@ class MainActivity : AppCompatActivity() {
 
 
 
+   
 
 
         }
+
+
 
         //alex page
 
