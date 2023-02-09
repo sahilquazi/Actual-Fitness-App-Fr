d6cf0d98112906e2c8fd258168bd529eec244c36
nguyenvo5 <tinhquoc218@gmail.com>
2023-02-09 18:49:14 -0500

    calendar

--- app/src/main/java/com/example/exerme/CalendarActivity.kt
+++ app/src/main/java/com/example/exerme/CalendarActivity.kt
@@ -7,14 +7,12 @@ import android.widget.TextView
 
 class CalendarActivity : AppCompatActivity() {
     var calendarView = findViewById<CalendarView>(R.id.calendarView)
-    var textView = findViewById<TextView>(R.id.textView2)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
         calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             val today = (dayOfMonth.toString() + "-" + (month + 1) + "-" + year)
-            textView.text = today
 
         }
     }
