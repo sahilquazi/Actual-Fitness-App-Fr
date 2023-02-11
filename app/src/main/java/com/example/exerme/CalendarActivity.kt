1178166d921bcd9637c7a9e5e171658bf567242a
nguyenvo5 <tinhquoc218@gmail.com>
2023-02-11 00:57:19 -0500

    calendar daily reminder test

--- app/src/main/java/com/example/exerme/CalendarActivity.kt
+++ app/src/main/java/com/example/exerme/CalendarActivity.kt
@@ -2,18 +2,29 @@ package com.example.exerme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CalendarView
-import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class CalendarActivity : AppCompatActivity() {
-    var calendarView = findViewById<CalendarView>(R.id.calendarView)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
-        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
-            val today = (dayOfMonth.toString() + "-" + (month + 1) + "-" + year)
 
+        val calendar = Calendar.getInstance()
+        val year = calendar[Calendar.YEAR]
+        val month = calendar[Calendar.MONTH]
+        val day = calendar[Calendar.DAY_OF_MONTH]
+        val today = year.toString() + "" + month + "" + day
+        val preferences = getSharedPreferences("PREFERENCES", 0)
+        val currentDay = preferences.getBoolean(today, false)
+
+        if (!currentDay) {
+            Toast.makeText(this, "go work out now", Toast.LENGTH_SHORT).show()
+            val editor = preferences.edit()
+            editor.putBoolean(today, true)
+            editor.apply()
+        } else {
+            Toast.makeText(this, "you already worked out", Toast.LENGTH_SHORT).show()
         }
     }
 }
