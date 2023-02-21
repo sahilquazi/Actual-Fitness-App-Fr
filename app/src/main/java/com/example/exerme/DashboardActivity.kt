6b4700597a2179afe059cabe7b4afb0301cee019
sahilquazi <121276074+sahilquazi@users.noreply.github.com>
2023-02-20 19:15:01 -0500

    files yay

--- app/src/main/java/com/example/exerme/DashboardActivity.kt
+++ app/src/main/java/com/example/exerme/DashboardActivity.kt
@@ -1,28 +1,41 @@
 package com.example.exerme
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+
 
 class DashboardActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        var prefs = PreferenceManager
+            .getDefaultSharedPreferences(this)
+        if (!prefs.getBoolean("firstTime", false)) {
+            val editor = prefs.edit()
+            editor.putBoolean("firstTime", true)
+            editor.commit()
+        }
+        if (prefs == true) {
+            setContentView(R.layout.quizpage)
+        }
+
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_dashboard)
 
         val dashboardToolbar = null
         setSupportActionBar(dashboardToolbar)
 
-        val buttonClick = findViewById<Button>(R.id.rahButton)
-        buttonClick.setOnClickListener {
-            setContentView(R.layout.quizpage)
+            val buttonClick = findViewById<Button>(R.id.rahButton)
+            buttonClick.setOnClickListener {
+                setContentView(R.layout.quizpage)
+            }
+
+            val buttonClick2 = findViewById<Button>(R.id.calendarButton)
+            buttonClick2.setOnClickListener {
+                setContentView(R.layout.activity_calendar)
+            }
+
+
         }
-
-        val buttonClick2 = findViewById<Button>(R.id.calendarButton)
-        buttonClick2.setOnClickListener {
-            setContentView(R.layout.activity_calendar)
-        }
-
-
     }
-}
