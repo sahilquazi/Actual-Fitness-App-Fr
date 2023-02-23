6ce91289ab1297cb798ba7730c7bab14451531ac
nguyenvo5 <tinhquoc218@gmail.com>
2023-02-22 20:11:32 -0500

    first part of shared preferences

--- app/src/main/java/com/example/exerme/QuizPageActivity.kt
+++ app/src/main/java/com/example/exerme/QuizPageActivity.kt
@@ -1,30 +1,36 @@
 package com.example.exerme
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.edit
 
 class QuizPageActivity : AppCompatActivity() {
-
-        private var pref: SharedPreferences = getSharedPreferences("mypref", MODE_PRIVATE)
 
         override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
             super.onCreate(savedInstanceState, persistentState)
             setContentView(R.layout.quizpage)
-            if (pref.getBoolean("firstTime", true)) { // use default of true
-                pref.edit { putBoolean("firstTime", false) } // persist value of false
-                // show T&C dialog that calls goToMain() when accepted
-            } else {
-                goToMain()
-            }
-        }
 
-        private fun goToMain() {
+            val nameQuiz = findViewById<TextView>(R.id.editTextTextPersonName2)
+            val emailQuiz = findViewById<TextView>(R.id.editTextTextEmailAddress2)
+            val buttonQuiz = findViewById<Button>(R.id.button2)
+
+            val preferences = getSharedPreferences("PREFERENCES", 0)
+
+            buttonQuiz.setOnClickListener {
+                val name = nameQuiz.text.toString()
+                val email = emailQuiz.text.toString()
+
+                val editor = preferences.edit()
+                editor.putString("nameQuiz", name)
+                editor.putString("emailQuiz", email)
+                editor.apply()
+            }
+
             val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
-            finish() // close this activity since we don't want it in back stack
+            finish()
         }
-    }
+}
