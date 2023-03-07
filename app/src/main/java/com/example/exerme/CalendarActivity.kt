dda7f4915fa98ab1c81d2e5df48d3682afc625da
sahilquazi <121276074+sahilquazi@users.noreply.github.com>
2023-03-06 19:31:03 -0500

    notification test

--- app/src/main/java/com/example/exerme/CalendarActivity.kt
+++ app/src/main/java/com/example/exerme/CalendarActivity.kt
@@ -5,7 +5,24 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+
+
 class CalendarActivity : AppCompatActivity() {
+
+
+    var streakSaverYesterday = 5
+    var StreakSaverToday = streakSaverYesterday + 1
+
+    var builder = NotificationCompat.Builder(this, CHANNEL_ID)
+        .setSmallIcon(R.drawable.notification_icon)
+        .setContentTitle("My notification")
+        .setContentText("Lets goo your streak is $StreakSaverToday" )
+        .setStyle(NotificationCompat.BigTextStyle()
+            .bigText("Much longer text that cannot fit one line..."))
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
@@ -20,5 +37,12 @@ class CalendarActivity : AppCompatActivity() {
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
             supportActionBar!!.setDisplayShowHomeEnabled(true)
         }
+
+
+        if streakSaverToday > streakSaverYesterday {
+
+
+        }
+
     }
 }
