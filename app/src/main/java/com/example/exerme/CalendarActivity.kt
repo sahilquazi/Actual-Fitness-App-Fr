82ed9be8da4dc51b097d3c7c85bab3aaf7374fe0
nguyenvo5 <tinhquoc218@gmail.com>
2023-03-06 19:41:36 -0500

    workout notification

--- app/src/main/java/com/example/exerme/CalendarActivity.kt
+++ app/src/main/java/com/example/exerme/CalendarActivity.kt
@@ -1,31 +1,25 @@
 package com.example.exerme
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
-
+import com.example.exerme.databinding.ActivityCalendarBinding
+import java.util.*
 
 class CalendarActivity : AppCompatActivity() {
-
-
-    var streakSaverYesterday = 5
-    var StreakSaverToday = streakSaverYesterday + 1
-
-    var builder = NotificationCompat.Builder(this, CHANNEL_ID)
-        .setSmallIcon(R.drawable.notification_icon)
-        .setContentTitle("My notification")
-        .setContentText("Lets goo your streak is $StreakSaverToday" )
-        .setStyle(NotificationCompat.BigTextStyle()
-            .bigText("Much longer text that cannot fit one line..."))
-        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-
-
+    private lateinit var binding : ActivityCalendarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calendar)
+        binding = ActivityCalendarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        createNotificationChannel()
 
         val backButton = findViewById<Button>(R.id.btnQuiz)
         backButton.setOnClickListener {
@@ -33,16 +27,51 @@ class CalendarActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        if (supportActionBar != null){
+        binding.btnNotification.setOnClickListener {
+            sendNotification()
+        }
+
+        if (supportActionBar != null) {
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
             supportActionBar!!.setDisplayShowHomeEnabled(true)
         }
+    }
 
+    private fun sendNotification() {
+        val intent = Intent(this, Notification:: class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            this,
+            1,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
 
-        if streakSaverToday > streakSaverYesterday {
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val time = getDate()
+        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, time, pendingIntent)
+    }
 
+    private fun getDate(): Long {
+        val calendar = Calendar.getInstance()
+        val minute = binding.timePicker.minute
+        val hour = binding.timePicker.hour
+        val day = binding.datePicker.dayOfMonth
+        val month = binding.datePicker.month
+        val year = binding.datePicker.year
 
+        calendar.set(year, month, day, hour, minute)
+        return calendar.timeInMillis
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "Notification Channel"
+            val channelDesc = "Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel("channel", channelName, importance)
+            channel.description = channelDesc
+
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
         }
-
     }
 }
