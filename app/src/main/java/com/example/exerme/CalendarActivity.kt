4eda56c777abae99142631aeff9de576d2369b7a
sahilquazi <121276074+sahilquazi@users.noreply.github.com>
2023-03-07 19:14:48 -0500

    notification test

--- app/src/main/java/com/example/exerme/CalendarActivity.kt
+++ app/src/main/java/com/example/exerme/CalendarActivity.kt
@@ -10,16 +10,13 @@ import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.example.exerme.databinding.ActivityCalendarBinding
+import androidx.core.app.NotificationCompat
 import java.util.*
 
 class CalendarActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityCalendarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityCalendarBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        createNotificationChannel()
+
 
         val backButton = findViewById<Button>(R.id.btnQuiz)
         backButton.setOnClickListener {
@@ -27,9 +24,6 @@ class CalendarActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        binding.btnNotification.setOnClickListener {
-            sendNotification()
-        }
 
         if (supportActionBar != null) {
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
@@ -37,41 +31,15 @@ class CalendarActivity : AppCompatActivity() {
         }
     }
 
-    private fun sendNotification() {
-        val intent = Intent(this, Notification:: class.java)
-        val pendingIntent = PendingIntent.getBroadcast(
-            this,
-            1,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
 
-        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val time = getDate()
-        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, time, pendingIntent)
-    }
+    var StreakSaverToday = 0
 
-    private fun getDate(): Long {
-        val calendar = Calendar.getInstance()
-        val minute = binding.timePicker.minute
-        val hour = binding.timePicker.hour
-        val day = binding.datePicker.dayOfMonth
-        val month = binding.datePicker.month
-        val year = binding.datePicker.year
 
-        calendar.set(year, month, day, hour, minute)
-        return calendar.timeInMillis
-    }
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelName = "Notification Channel"
-            val channelDesc = "Description"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel("channel", channelName, importance)
-            channel.description = channelDesc
-
-            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
+    var builder = NotificationCompat.Builder(this, 1)
+        .setSmallIcon(R.drawable.notification_icon)
+        .setContentTitle("My notification")
+        .setContentText("Lets goo your streak is $StreakSaverToday" )
+        .setStyle(NotificationCompat.BigTextStyle()
+            .bigText("Click this to continue your streak!"))
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 }
