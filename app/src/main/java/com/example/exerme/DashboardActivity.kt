5d7a207a4c11b5dfe48e97ada0f01a8bc25505c2
sahilquazi <121276074+sahilquazi@users.noreply.github.com>
2023-03-07 20:15:19 -0500

    youtube (no api javascript thing) test

--- app/src/main/java/com/example/exerme/DashboardActivity.kt
+++ app/src/main/java/com/example/exerme/DashboardActivity.kt
@@ -1,8 +1,16 @@
 package com.example.exerme
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.Button
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -16,6 +24,84 @@ class DashboardActivity : AppCompatActivity() {
         buttonCalendar.setOnClickListener {
             val intent = Intent(this, CalendarActivity::class.java)
             startActivity(intent)
+
+
+
+
+
+
+
+
+
+
+
+            @SuppressLint("SetJavaScriptEnabled")
+            class WebViewActivity : AppCompatActivity() {
+
+                private lateinit var webView: WebView
+
+                override fun onCreate(savedInstanceState: Bundle?) {
+                    super.onCreate(savedInstanceState)
+                    setContentView(R.layout.activity_dashboard)
+
+                    webView = findViewById(R.id.webview_player_view)
+                    webView.webViewClient = WebViewClient()
+                    webView.webChromeClient = CustomChromeClient() // Full-screen support
+                    val webSettings = webView.settings
+                    webSettings.javaScriptEnabled = true
+                    webSettings.allowFileAccess = true
+                    if (savedInstanceState == null) {
+                        webView.loadUrl("https://www.youtube.com/embed/YE7VzlLtp-4")
+                    }
+                }
+
+                private inner class CustomChromeClient : WebChromeClient() {
+                    private var mCustomView: View? = null
+                    private var mCustomViewCallback: CustomViewCallback? = null
+                    private var mOriginalOrientation = 0
+                    private var mOriginalSystemUiVisibility = 0
+                    override fun getDefaultVideoPoster(): Bitmap? {
+                        return if (mCustomView == null) {
+                            null
+                        } else BitmapFactory.decodeResource(applicationContext.resources, 2130837573)
+                    }
+
+                    override fun onHideCustomView() {
+                        (window.decorView as FrameLayout).removeView(mCustomView)
+                        mCustomView = null
+                        window.decorView.systemUiVisibility = mOriginalSystemUiVisibility
+                        requestedOrientation = mOriginalOrientation
+                        mCustomViewCallback!!.onCustomViewHidden()
+                        mCustomViewCallback = null
+                    }
+
+                    override fun onShowCustomView(
+                        paramView: View?,
+                        paramCustomViewCallback: CustomViewCallback?
+                    ) {
+                        if (mCustomView != null) {
+                            onHideCustomView()
+                            return
+                        }
+                        mCustomView = paramView
+                        mOriginalSystemUiVisibility = window.decorView.systemUiVisibility
+                        mOriginalOrientation = requestedOrientation
+                        mCustomViewCallback = paramCustomViewCallback
+                        (window.decorView as FrameLayout).addView(mCustomView, FrameLayout.LayoutParams(-1, -1))
+                        window.decorView.systemUiVisibility = 3846 or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    }
+                }
+
+                override fun onSaveInstanceState(outState: Bundle) {
+                    super.onSaveInstanceState(outState)
+                    webView.saveState(outState)
+                }
+
+                override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+                    super.onRestoreInstanceState(savedInstanceState)
+                    webView.restoreState(savedInstanceState)
+                }
+            }
         }
     }
 }
