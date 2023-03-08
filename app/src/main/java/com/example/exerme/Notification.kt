e4c313a8e8590b48fbb2c3490aeb34c5a5844c07
nguyenvo5 <tinhquoc218@gmail.com>
2023-03-07 20:21:14 -0500

    workout notification fix

--- app/src/main/java/com/example/exerme/Notification.kt
+++ app/src/main/java/com/example/exerme/Notification.kt
@@ -1,20 +1,32 @@
 package com.example.exerme
 
+import android.Manifest
 import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class Notification : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        val notification = NotificationCompat.Builder(this, "channel")
+    override fun onReceive(context: Context, intent: Intent?) {
+        val notification = NotificationCompat.Builder(context, "2")
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle("Exerme")
             .setContentText("Go work out now!")
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
-        val manager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        manager.notify(1, notification)
+        with(NotificationManagerCompat.from(context)) {
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return
+            }
+            notify(1, notification.build())
+        }
     }
 }
