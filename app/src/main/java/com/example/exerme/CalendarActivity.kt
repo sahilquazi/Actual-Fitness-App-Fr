615f5c878819a0b0e768fca4434f804e032e2bfe
Alex5563 <alex.huang3651@g.gcpsk12.org>
2023-03-14 20:36:02 -0400

    change

--- app/src/main/java/com/example/exerme/CalendarActivity.kt
+++ app/src/main/java/com/example/exerme/CalendarActivity.kt
@@ -1,14 +1,27 @@
 package com.example.exerme
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.exerme.databinding.ActivityCalendarBinding
+import java.util.*
+
 
 class CalendarActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCalendarBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_calendar)
+        binding = ActivityCalendarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        createNotificationChannel()
 
         val backButton = findViewById<Button>(R.id.btnQuiz)
         backButton.setOnClickListener {
@@ -16,9 +29,49 @@ class CalendarActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        if (supportActionBar != null){
+        binding.btnNotification.setOnClickListener {
+            sendNotification()
+            Toast.makeText(this, "Reminder has been set", Toast.LENGTH_SHORT).show()
+        }
+
+        if (supportActionBar != null) {
             supportActionBar!!.setDisplayHomeAsUpEnabled(true)
             supportActionBar!!.setDisplayShowHomeEnabled(true)
         }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = "Notification"
+            val channelDesc = "Schedules reminders at a specific time to work out"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(Notification.channelID, channelName, importance).apply {
+                description = channelDesc
+            }
+
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun getDate(): Long {
+        val calendar = Calendar.getInstance()
+        val minute = binding.timePicker.minute
+        val hour = binding.timePicker.hour
+        val day = binding.datePicker.dayOfMonth
+        val month = binding.datePicker.month
+        val year = binding.datePicker.year
+
+        calendar.set(year, month, day, hour, minute)
+        return calendar.timeInMillis
+    }
+
+    private fun sendNotification() {
+        val intent = Intent(this, Notification::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(this, Notification.notificationId, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val time = getDate()
+        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, time, pendingIntent)
     }
 }
